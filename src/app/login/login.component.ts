937ac81780ca4da8a7124e47f120dfccabdd9cc6
Ayush Pandit <apandit@asite.com>
2023-03-28 19:31:32 +0530

    modified  login,interview components ,fixed typo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl ,FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatFabButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  
-  constructor() { 
-
+  public showPassword: boolean = false;
+  constructor() {}
+  loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+    ]),
+  });
+  loginUser() {
+    console.log(this.loginForm.value);
   }
-  loginForm =  new FormGroup({
-    username: new FormControl("",[Validators.required,Validators.pattern('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$')]),
-    password: new FormControl("",[Validators.required,Validators.minLength(7)]),
-  })
-  loginUser(){
-    console.log(this.loginForm.value)    
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
-  ngOnInit(): void {
-}
-
+  ngOnInit(): void {}
 }
