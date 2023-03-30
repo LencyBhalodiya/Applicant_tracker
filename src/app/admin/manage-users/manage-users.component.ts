ab516f3136a201781a0cb765869dd652772723d9
Rahil Acharya <racharya@asite.com>
2023-03-30 15:44:53 +0530

    manage-user component

--- src/app/admin/manage-users/manage-users.component.ts
+++ src/app/admin/manage-users/manage-users.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ManageUsersComponent {
   name: string = 'rahil';
+  display = 'none';
+
+  users: { name: string; email: string; status: string }[] = [
+    { name: 'rahil', email: 'abc@gmail.com', status: 'Active' },
+    { name: 'rahil1', email: 'abcdefd@gmail.com', status: 'Inactive' },
+    { name: 'rahil12', email: 'abcxyz@gmail.com', status: 'Active' },
+    { name: 'rahil12', email: 'abcxyz@gmail.com', status: 'Active' },
+    { name: 'rahil12', email: 'abcxyz@gmail.com', status: 'Active' },
+  ];
+
+  openModal() {
+    this.display = 'block';
+  }
+
+  onCloseHandled() {
+    this.display = 'none';
+  }
 }
