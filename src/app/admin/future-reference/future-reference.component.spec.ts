8d9a1d98148c98dbff7eb860a9fc493abfbde82a
Shaikh Mohammed Muzammil <smohammed@asite.com>
2023-04-23 23:35:55 +0530

    Changes to spec

--- src/app/admin/future-reference/future-reference.component.spec.ts
+++ src/app/admin/future-reference/future-reference.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from 'src/app/shared/material-design/material-design.module';
 
 import { FutureReferenceComponent } from './future-reference.component';
 
@@ -8,7 +10,8 @@ describe('FutureReferenceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FutureReferenceComponent ]
+      declarations: [ FutureReferenceComponent ],
+      imports : [ MaterialDesignModule , BrowserAnimationsModule ]
     })
     .compileComponents();
 
