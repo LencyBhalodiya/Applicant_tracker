fb1df8b986dfcc0f9ed665f8ede9e211f656e86c
lency <lbhalodiya@asite.com>
2023-04-23 16:59:54 +0530

    fture ref

--- src/app/admin/future-reference/future-reference.component.ts
+++ src/app/admin/future-reference/future-reference.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RefServicesService } from './services/ref-services.service';
 
 @Component({
   selector: 'app-future-reference',
   templateUrl: './future-reference.component.html',
   styleUrls: ['./future-reference.component.css']
 })
-export class FutureReferenceComponent {
+export class FutureReferenceComponent implements OnInit {
+   data:any = [{}]
 
+  constructor(private _refService: RefServicesService,private router: Router ) { 
+  }
+
+  getData(){
+   this._refService.futureRef().subscribe((res)=>{
+    this.data = res;
+    console.log(this.data);
+    
+   })
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
+  onRoute(id:number)
+  {
+    this.router.navigate(['applicant/profile/']);
+  }
 }
--- src/app/admin/future-reference/services/ref-services.service.ts
+++ src/app/admin/future-reference/services/ref-services.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,9 @@ export class RefServicesService {
 
   constructor(private http: HttpClient) { }
   private url  = 'http://192.168.102.92:8002/main/api/admin/getAllFutureRef';
+  searchUrl = this.url;
 
   futureRef() {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
   }
 }
