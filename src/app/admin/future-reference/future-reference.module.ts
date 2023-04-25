6be1b71eb1dd7af4a2f97558eb22c0fa9b6b2d9d
lency <lbhalodiya@asite.com>
2023-04-25 19:32:46 +0530

    design changes

--- src/app/admin/future-reference/future-reference.module.ts
+++ src/app/admin/future-reference/future-reference.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { FutureReferenceRoutingModule } from './future-reference-routing.module';
 import { FutureReferenceComponent } from './future-reference.component';
 import { MaterialDesignModule } from 'src/app/shared/material-design/material-design.module';
+import { ColorPipe } from './services/color.pipe';
 
 
 @NgModule({
   declarations: [
-    FutureReferenceComponent
+    FutureReferenceComponent,
+    ColorPipe
   ],
   imports: [
     MaterialDesignModule,
     CommonModule,
-    FutureReferenceRoutingModule
+    FutureReferenceRoutingModule,
   ]
 })
 export class FutureReferenceModule { }
