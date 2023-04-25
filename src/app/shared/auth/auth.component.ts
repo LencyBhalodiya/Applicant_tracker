f8feccdf1c62f2bb52bc2ea1e0e33c918ee04d88
Ayush Pandit <apandit@asite.com>
2023-04-25 16:19:29 +0530

    auth comp changes

--- src/app/shared/auth/auth.component.ts
+++ src/app/shared/auth/auth.component.ts
@@ -6,68 +6,61 @@ import { AuthService } from './auth-services/auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
   public showPassword: boolean = false;
   constructor(private auth: AuthService) {}
- loginForm = new FormGroup({
-   email: new FormControl('', [
-     Validators.required,
-     Validators.email,
-   ]),
-   password: new FormControl('', [
-     Validators.required,
-     Validators.minLength(7),
-   ]),
- });
- loginUser() {
-   this.auth.signIn(this.loginForm.value)
- }
- public togglePasswordVisibility(): void {
-   this.showPassword = !this.showPassword;
- }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(16),
+    ]),
+  });
+  loginUser() {
+    this.auth.signIn(this.loginForm.value);
+  }
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
- ngOnInit(): void {}
+  ngOnInit(): void {}
 
- registerForm = new FormGroup({
-   firstname : new FormControl('',[
-     Validators.required
-   ]),
-   lastname : new FormControl('',[
-     Validators.required
-   ]),
-   email: new FormControl('', [
-     Validators.required,
-     Validators.pattern('[a-z0-9]+@[a-z]+.[a-z]{2,3}'),
-   ]),
-   password: new FormControl('', [
-    Validators.required,
-    Validators.minLength(7),
-  ]),
-   phoneNumber : new FormControl('',[
-     Validators.required,
-     Validators.maxLength(10),
-     Validators.minLength(10)
-   ]),
-   dob: new FormControl('',[
+  registerForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-z0-9]+@[a-z]+.[a-z]{2,3}'),
+    ]),
+    password: new FormControl('', [
+      Validators.pattern(
+        /(?=.*[a-z])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+      ),
+      Validators.maxLength(16),
+      Validators.minLength(8),
+      Validators.required,
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{10}'),
+    ]),
+    dob: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+  });
+  registerUser() {
+    var datePipe = new DatePipe('en-US');
+    var setDob = datePipe.transform(this.registerForm.value.dob, 'yyyy-MM-dd');
 
-   ]),
-   gender :new FormControl('',[
-     Validators.required
-   ])
- })
- registerUser(){
-  var datePipe = new DatePipe('en-US');
-  var setDob = datePipe.transform(this.registerForm.value.dob, 'yyyy-MM-dd');
-   
-  setDob = this.registerForm.value.dob = setDob;
-  console.log(this.registerForm.value);
-  
-   this.auth.signUp(this.registerForm.value)
- }
- dispalyHide(signup:HTMLElement,signin:HTMLElement){
-   signup.style.display="none"
-   signin.style.display="flex"
- }
+    setDob = this.registerForm.value.dob = setDob;
+    console.log(this.registerForm.value);
+
+    this.auth.signUp(this.registerForm.value);
+  }
+  dispalyHide(signup: HTMLElement, signin: HTMLElement) {
+    signup.style.display = 'none';
+    signin.style.display = 'flex';
+  }
 }
